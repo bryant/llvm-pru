6acdd45e46f300742a4eddde6697f704b82a85ea
bryant <bryant@defrag.in>
2016-05-02 07:29:27 -0400

    pragma packed structs in arg tests

--- test/packed_args.c
+++ test/packed_args.c
@@ -1,5 +1,7 @@
 // RUN: %clang -O3 -S -o - -target pru -mllvm -bbo-combiner2 %s | FileCheck %s
 
+#pragma pack(1)
+
 // we expect `a`, `b`, `c` to be packed into r14.b0, r15, and r14.w1,
 // respectively. furthermore, we expect the codegen to be efficient enough to
 // subtract directly to the return value register, r14.
