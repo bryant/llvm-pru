cc4cddb9840c83cb0474a23f0f0ea663c8ede64a
bryant <bryant@defrag.in>
2016-05-02 04:12:25 -0400

    add test for packed args

---
+++ test/packed_args.c
@@ -0,0 +1,11 @@
+// RUN: %clang -O3 -S -o - -target pru %s | FileCheck %s
+
+// we expect `a`, `b`, `c` to be packed into r14.b0, r15, and r14.w1,
+// respectively. furthermore, we expect the codegen to be efficient enough to
+// subtract directly to the return value register, r14.
+unsigned int add_sub(unsigned char a, unsigned int b, unsigned short c) {
+    // CHECK-LABEL: add_sub:
+    // CHECK-DAG: ADD {{[^,]+}}, r14.b0, r15
+    // CHECK-DAG: SUB r14, {{[^,]+}}, r14.w1
+    return a + b - c;
+}
