329c89b5087ba6bf06c294476dc5bb06c05ec348
bryant <bryant@defrag.in>
2016-05-03 19:50:25 -0400

    add function call tests

---
+++ test/function_call.c
@@ -0,0 +1,17 @@
+// RUN: %clang -O3 -S -o - -target pru -fverbose-asm -mllvm -bbo-combiner2 %s \
+// RUN:     | FileCheck %s
+
+unsigned int weird_jump(unsigned int (*jump_point)()) {
+    // CHECK-LABEL: weird_jump:
+    // CHECK: JAL r3.w2, r14
+    return 3 + (*jump_point)();
+}
+
+// note that call addresses are technically sixteen-bit instruction sequence
+// counters, so below is technically allowed (in fact, clpru even permits `JAL`
+// to be paired with eight-bit registers).
+unsigned int call_reg16(unsigned short instr_addr) {
+    // CHECK-LABEL: call_reg16:
+    return 3 + ((unsigned int (*)()) instr_addr)();
+}
+
